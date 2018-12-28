69941c923b3ad226973a9a8682b3fcc5f5b91c5c
jefferson nunes fonseca <jeffersonnunesfonseca@gmail.com>
2018-12-28 21:58:16 -0200

    abilitando rewrite no dockerfile, para habilitar rotas

--- estudos/routes/web.php
+++ estudos/routes/web.php
@@ -11,6 +11,34 @@
 |
 */
 
+//pega qualquer tipo de requisição
+// Route::any('/any', function () {
+//     return "ROTA ANY";
+// });
+
+// //independente de onde vem o /match, fica flexivel
+// Route::match(['get','post'],'/match', function () {
+//     return "ROTA MATCH";
+// });
+
+// //quando é post
+// Route::post('/empresa', function () {
+//     return "ROTA POST";
+// });
+
+//nomeando a rota. deste modo, quando houver alteração no caminho. não será necessário mudar em outra parte do código
+Route::get('/nome/nome1/nome2', function () {
+    return view("welcome");
+})->name('rota.nomeada');
+
+// Route::get('/', function () {
+//     return "aa";
+// });
+
+
+//pegando rota nomeada
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('rota.nomeada');
 });
+
+?>
