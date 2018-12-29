81a4ac5f2cf3e195ec44567b82f472c95a320476
jefferson nunes fonseca <jeffersonnunesfonseca@gmail.com>
2018-12-29 13:24:35 -0200

    seeders e alguns comandos do artisan

--- estudos/database/migrations/2018_12_29_145242_create_produtos_table.php
+++ estudos/database/migrations/2018_12_29_145242_create_produtos_table.php
@@ -18,7 +18,7 @@ class CreateProdutosTable extends Migration
             $table->string('name',150);
             $table->integer('number');
             $table->boolean('active');
-            $table->string('image',200);
+            $table->string('image',200)->nullable();
             $table->enum('category',['eletronicos','moveis','limpeza']);
             $table->text('description');
             $table->timestamps();
